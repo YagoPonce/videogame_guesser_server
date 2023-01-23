856c0ba1fd83b059f18b7ef4d7f88f22cbadf6ec
Yago <yagoponcem@gmail.com>
2023-01-23 11:53:44 +0100

    form is working!

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -119,7 +119,7 @@ router.post("/login", (req, res, next) => {
 });
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get("/verify", isAuthenticated, (req, res, next) => {
+router.get("/verify"/*, isAuthenticated*/, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and is made available on `req.payload`
   // console.log(`req.payload`, req.payload);
