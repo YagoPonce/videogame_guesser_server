31a9707c4d679162a4c6c64df590ba56c4ebe88c
Yago <yagoponcem@gmail.com>
2023-01-24 13:25:06 +0100

    fixing image nameless cloudinary

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 
 const uploader = require("../config/cloudinary.config");
 
+router.post("/nameless", uploader.single("image"), (req, res, next) => {
+  if (req.file === undefined) {
+    res.status(400).json("problems when uploading the img");
+    return;
+  }
+
+  res.status(200).json({ image: req.file.path });
+});
+
 router.post("/", uploader.single("image"), (req, res, next) => {
   if (req.file === undefined) {
     res.status(400).json("problems when uploading the img");
